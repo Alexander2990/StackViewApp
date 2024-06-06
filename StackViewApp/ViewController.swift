e41fa2ca0d770f2fb27dc249a61ffb3dea7e03bc
Alexander <Alexander29_90@icloud.com>
2024-06-06 21:33:48 +0500

    Initial Commit

---
+++ StackViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewApp
+//
+//  Created by Александр on 06.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
